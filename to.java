7cfaedf41e66934bf9c79bbe42d9871274d7d409
Palak Gupta <palakgupta955442@gmail.com>
2025-07-26 16:34:54 +0530

    first commit

---
+++ to.java
@@ -0,0 +1,20 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 10;  // First number
+        int num2 = 20;  // Second number
+
+        int sum = num1 + num2;  // Add the numbers
+
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 10;  // First number
+        int num2 = 20;  // Second number
+
+        int sum = num1 + num2;  // Add the numbers
+
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
